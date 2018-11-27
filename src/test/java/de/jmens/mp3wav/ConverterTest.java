f6c4a33dd02374e737afa671f7196b6ef89b48da
Clemens von Musil <clemens@vonmusil.de>
2018-11-27 08:33:10 +0100

    feat: add converter

--- src/test/java/de/jmens/mp3wav/ConverterTest.java
+++ src/test/java/de/jmens/mp3wav/ConverterTest.java
@@ -1,25 +1,30 @@
 package de.jmens.mp3wav;
 
 import static de.jmens.mp3wav.Converter.convertFrom;
-import static java.nio.file.Files.readAllBytes;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import javax.sound.sampled.AudioFormat;
 import org.junit.Test;
 
 public class ConverterTest {
+
 	@Test
 	public void testConverer() throws Exception {
 
-		final ByteArrayInputStream inputStream = new ByteArrayInputStream(readAllBytes(Paths.get("/tmp/input.mp3")));
-		final ByteArrayOutputStream output = new ByteArrayOutputStream();
-		final AudioFormat audioFormat = new AudioFormat(44100, 8, 1, true, false);
+		try (
+				final InputStream inputStream = getClass().getResourceAsStream("/test.mp3");
+				final ByteArrayOutputStream output = new ByteArrayOutputStream();
+		) {
+			final AudioFormat audioFormat = new AudioFormat(44100, 8, 1, true, false);
 
-		convertFrom(inputStream).withFormat(audioFormat).to(output);
+			convertFrom(inputStream).withFormat(audioFormat).to(output);
 
-		Files.write(Paths.get("/tmp/output.wav"), output.toByteArray());
+//			Files.write(Paths.get("/tmp/output.wav"), output.toByteArray());
+		} finally {
+			// intentionally left blank
+		}
 	}
 }
